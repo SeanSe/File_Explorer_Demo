2e91a0969f2d9cfc70e1664a52994c121666683a
Sean <ses0384@my.londonmet.ac.uk>
2016-05-07 12:43:45 +0100

    Initial commit

---
+++ app/src/main/java/com/project/sean/file_explorer_demo/MainActivity.java
@@ -0,0 +1,175 @@
+package com.project.sean.file_explorer_demo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    Button buttonOpenDialog;
+    Button buttonUp;
+    TextView textFolder;
+
+    String KEY_TEXTPSS = "TEXTPSS";
+    static final int CUSTOM_DIALOG_ID = 0;
+    ListView dialog_ListView;
+
+    File root;
+    File curFolder;
+
+    private List<String> fileList = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonOpenDialog = (Button) findViewById(R.id.opendialog);
+        buttonOpenDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(CUSTOM_DIALOG_ID);
+            }
+        });
+
+        isStoragePermissionGranted();
+
+        root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+        curFolder = root;
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        Dialog dialog = null;
+
+        switch (id) {
+            case CUSTOM_DIALOG_ID:
+                dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.dialoglayout);
+                dialog.setTitle("Custom Dialog");
+                dialog.setCancelable(true);
+                dialog.setCanceledOnTouchOutside(true);
+
+                textFolder = (TextView) dialog.findViewById(R.id.folder);
+                buttonUp = (Button) dialog.findViewById(R.id.up);
+                buttonUp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ListDir(curFolder.getParentFile());
+                    }
+                });
+
+                dialog_ListView = (ListView) dialog.findViewById(R.id.dialoglist);
+                dialog_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        File selected = new File(fileList.get(position));
+                        if(selected.isDirectory()) {
+                            ListDir(selected);
+                        } else {
+                            Toast.makeText(MainActivity.this, selected.toString() + " selected",
+                                    Toast.LENGTH_LONG).show();
+                            dismissDialog(CUSTOM_DIALOG_ID);
+                        }
+                    }
+                });
+
+                break;
+        }
+        return dialog;
+    }
+
+    @Override
+    protected void onPrepareDialog(int id, Dialog dialog) {
+        super.onPrepareDialog(id, dialog);
+        switch (id) {
+            case CUSTOM_DIALOG_ID:
+                ListDir(curFolder);
+                break;
+        }
+    }
+
+    void ListDir(File f) {
+        if(f.equals(root)) {
+            buttonUp.setEnabled(false);
+        } else {
+            buttonUp.setEnabled(true);
+        }
+
+        curFolder = f;
+        textFolder.setText(f.getPath());
+
+        File[] files = f.listFiles();
+        fileList.clear();
+
+        for(File file : files) {
+            fileList.add(file.getPath());
+        }
+
+        ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, fileList);
+        dialog_ListView.setAdapter(directoryList);
+    }
+
+
+    /**
+     * Checks the version of android being used, if equal to or higher than 23
+     * file permissions must be first checked.
+     * @return
+     */
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG, "Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG, "Permission is granted");
+            return true;
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+            //resume tasks needing this permission
+            //kills the app process, restarting the app
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+    }
+}
